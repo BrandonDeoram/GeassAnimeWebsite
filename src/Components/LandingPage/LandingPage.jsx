bb6adb4f6a39a3a87564c351676a1258829b3a5a
BrandonDeoram <brandondeoram8931@gmail.com>
2023-02-18 22:32:33 -0500

    Imporve flex for landingPage

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -4,6 +4,9 @@ export default function LandingPage() {
     return (
         <div className={styles.container}>
             <img src="http://www.clker.com/cliparts/C/8/5/F/s/V/geass-kr-md.png" alt="no pic" className={styles.geassImage} />
+            {/* <div className={styles.searchBackground}>
+                Search
+            </div> */}
             <div className={styles.linkContainer}>
                 <a href="Home" className={styles.link}>
                     Home
@@ -17,6 +20,10 @@ export default function LandingPage() {
                 <a href="Top" className={styles.link}>
                     Top 10
                 </a>
+            </div>
+           
+            <div>
+                    Search     
             </div>
         </div>
     )
