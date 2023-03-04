266cc46de78813057ad79108162a71b4c7512499
BrandonDeoram <brandondeoram8931@gmail.com>
2023-03-04 00:52:24 -0500

    added genres

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { getTopAnimes } from './backend/api';
+import { getTopAnimes, getActionAnimes } from './backend/api';
 import CarouselComp from './Components/Carousels/CarouselComp';
 import NavBar from './Components/NavBar/NavBar';
 import LandingPage from './Components/LandingPage/LandingPage';
 import WrapCarousel from './Components/Wrappers/WrapCarousel';
 import TitleWCarousel from './Components/TitleWCarousel/TitleWCarousel';
+
 function App() {
   //Make API calls and populate carousel with the animelist 
   const [topAnime, setTopAnime] = useState([]);
+  const [actionAnime, setActionAnime] = useState([]);
+
 
   useEffect(() => {
     getTopAnimes().then((res) => setTopAnime(res.data));
+    getActionAnimes().then((res) => setActionAnime(res.data));
 
   }, [])
   return (
@@ -19,8 +23,8 @@ function App() {
         <NavBar></NavBar>
         <LandingPage></LandingPage>
         <WrapCarousel>
-          <TitleWCarousel title={"Novelty"} animes={topAnime}></TitleWCarousel>
-          <TitleWCarousel title={"Shounen"} animes={topAnime}></TitleWCarousel>
+          <TitleWCarousel title={"Top"} animes={topAnime}></TitleWCarousel>
+          <TitleWCarousel title={"Action"} animes={actionAnime}></TitleWCarousel>
         </WrapCarousel>
 
         {/* <CarouselComp props={topAnime} /> */}
--- src/backend/api.js
+++ src/backend/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 // backtick - `
 const api = axios.create({
   baseURL: "http://localhost:5000/"
@@ -17,6 +18,16 @@ export const getTopAnimes = async () => {
     return error;
   }
 };
+export const getActionAnimes = async () => {
+  try {
+    const response = await jikanApi.get('anime?genres=1');
+    return response.data
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+
+}
 export const getSearchAnime = async (animeName) => {
   try {
     const response = await jikanApi.get(`anime?q=${animeName}&page=1`);
