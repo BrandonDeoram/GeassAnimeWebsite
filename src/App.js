ec18ba673fe9f46a4323923eac9c4fc015f4629b
BrandonDeoram <brandondeoram8931@gmail.com>
2023-03-06 22:17:15 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { getTopAnimes, getGenre } from './backend/api';
-import CarouselComp from './Components/Carousels/CarouselComp';
 import NavBar from './Components/NavBar/NavBar';
 import LandingPage from './Components/LandingPage/LandingPage';
 import WrapCarousel from './Components/Wrappers/WrapCarousel';
@@ -17,32 +16,41 @@ function App() {
 
 
   useEffect(() => {
-    getTopAnimes().then((res) => setTopAnime(res.data));
-    getGenre(1).then((res) => setActionAnime(res.data)).then(() => getGenre(2).then((res) => setAdventureAnime(res.data))).then(() => getGenre(5).then((res) => setAdvantAnime(res.data)));
+    let isMounted = true;
+
+    // Make API calls and set state only if the component is mounted
+    if (isMounted) {
+      getTopAnimes().then((res) => setTopAnime(res.data));
+      getGenre(1).then((res) => setActionAnime(res.data))
+        .then(() => getGenre(2).then((res) => setAdventureAnime(res.data)))
+        .then(() => getGenre(5).then((res) => setAdvantAnime(res.data)));
+    }
+
+    // Return a cleanup function to be called when the component unmounts
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  return (
+    <div className="App">
+      <div id="content">
+        <NavBar></NavBar>
+        <LandingPage></LandingPage>
+        <WrapCarousel>
+          <TitleWCarousel title={"Top"} animes={topAnime}></TitleWCarousel>
+          <TitleWCarousel title={"Action"} animes={actionAnime}></TitleWCarousel>
+          <TitleWCarousel title={"Adventure"} animes={adventureAnime}></TitleWCarousel>
+          <TitleWCarousel title={"Advant Garde"} animes={advantAnime}></TitleWCarousel>
 
 
+        </WrapCarousel>
 
-}, [])
-return (
-  <div className="App">
-    <div id="content">
-      <NavBar></NavBar>
-      <LandingPage></LandingPage>
-      <WrapCarousel>
-        <TitleWCarousel title={"Top"} animes={topAnime}></TitleWCarousel>
-        <TitleWCarousel title={"Action"} animes={actionAnime}></TitleWCarousel>
-        <TitleWCarousel title={"Adventure"} animes={adventureAnime}></TitleWCarousel>
-        <TitleWCarousel title={"Advant Garde"} animes={advantAnime}></TitleWCarousel>
+        {/* <CarouselComp props={topAnime} /> */}
+      </div>
 
-
-      </WrapCarousel>
-
-      {/* <CarouselComp props={topAnime} /> */}
     </div>
 
-  </div>
-
-);
+  );
 }
 
 export default App;
