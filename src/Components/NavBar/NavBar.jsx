d73789819bc3980224ccbfb62a2b2a1418b68e78
BrandonDeoram <brandondeoram8931@gmail.com>
2023-03-07 21:46:30 -0500

    Added onclose for button

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import style from "./NavBar.module.css";
 
 const SignInModal = ({ onClose }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const modalRef = useRef(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,9 +12,21 @@ const SignInModal = ({ onClose }) => {
         onClose();
     }
 
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (modalRef.current && !modalRef.current.contains(e.target)) {
+                onClose();
+            }
+        };
+        window.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            window.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [onClose]);
+
     return (
         <div className={style.modal}>
-            <div className={style.modalContent}>
+            <div className={style.modalContent} ref={modalRef}>
                 <h2>Sign In</h2>
                 <form onSubmit={handleSubmit}>
                     <label>
@@ -47,7 +60,7 @@ export default function NavBar() {
         <>
             <ul className={style.ul}>
                 <li className={style.li}>
-                    <button className={style.signIn}  onClick={handleSignInClick}>Sign In</button>
+                    <button className={style.signIn} onClick={handleSignInClick}>Sign In</button>
                 </li>
             </ul>
             {showModal && <SignInModal onClose={handleCloseModal} />}
