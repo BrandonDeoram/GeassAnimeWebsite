6a3116314e319c90fa838cbc0ab18853c84e63b6
BrandonDeoram <brandondeoram8931@gmail.com>
2023-03-08 17:01:15 -0500

    made adjustments to card

--- src/Components/Carousels/CarouselComp.jsx
+++ src/Components/Carousels/CarouselComp.jsx
@@ -1,73 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import { getTopAnimes } from '../../backend/api'
+import React from "react";
 import styles from "./Carousels.module.css";
-import LargeAnimeCard from '../Cards/LargeAnimeCard';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from "@material-ui/icons/Delete";
+import LargeAnimeCard from "../Cards/LargeAnimeCard";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import IconButton from "@mui/material/IconButton";
 import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
 
-//Takes an AnimeList and turns it into a card 
+//Takes an AnimeList and turns it into a card
 export default function CarouselComp({ props }) {
-    let animes = props;
-    // anime['images']['jpg']['image_url']
-    /* {anime['title']} */
-    return (
-        <Splide aria-label="My Favorite Images" className={styles.carouselContainer} options={{
-            breakpoints: {
-                700: {
-                    perPage: 3,
-                    gap: 5
-                },
-                1000: {
-                    perPage: 5,
-                    gap: 20
-                },
-                1200: {
-                    perPage: 7,
-                    gap: 20
-                },
-                1400: {
-                    perPage: 8,
-                    gap: 10
-                },
-                2000: {
-                    perPage: 10,
-                    gap: 10,
-                },
-            },
+  let animes = props;
+  // anime['images']['jpg']['image_url']
+  /* {anime['title']} */
+  return (
+    <Splide
+      aria-label="My Favorite Images"
+      className={styles.carouselContainer}
+      options={{
+        breakpoints: {
+          700: {
+            perPage: 3,
+            gap: 5,
+          },
+          1000: {
+            perPage: 5,
+            gap: 20,
+          },
+          1200: {
+            perPage: 7,
+            gap: 20,
+          },
+          1400: {
+            perPage: 8,
+            gap: 10,
+          },
+          2000: {
+            perPage: 10,
+            gap: 10,
+          },
+        },
 
-            rewind: true,
-        }} >
-
-            {animes.map((anime, id) => {
-                return <SplideSlide key={id}>
-
-                    <div className="card">
-                        <a href={`/anime/${anime.mal_id}`}>
-                            <img src={anime['images']['jpg']['image_url']} />
-
-                            <div className="container2" >
-                                {anime['title']}
-                            </div>
-                        </a>
-                        <IconButton aria-label="delete" sx={{
-                            width: 50,
-                            color: 'white',
-                            zIndex: 200,
-                            position: 'absolute',
-                            right: '0%',
-                        }}>
-                            <LibraryAddIcon />
-                        </IconButton>
-                    </div>
-
-
-                </SplideSlide>
-
-            })}
-
-        </Splide>
-    );
+        rewind: true,
+      }}
+    >
+      {animes.map((anime, id) => {
+        return (
+          <SplideSlide key={id}>
+            <div className="card">
+              <a href={`/anime/${anime.mal_id}`}>
+                <img src={anime["images"]["jpg"]["image_url"]} alt={"loading"}/>
+                <div className="container2">{anime["title"]}</div>
+              </a>
+              <IconButton
+                aria-label="delete"
+                sx={{
+                  width: 50,
+                  color: "white",
+                  zIndex: 200,
+                  position: "absolute",
+                  right: "0%",
+                }}
+                onClick={() => {
+                    console.log(anime.mal_id);
+                }}
+              >
+                <LibraryAddIcon />
+              </IconButton>
+            </div>
+          </SplideSlide>
+        );
+      })}
+    </Splide>
+  );
 }
