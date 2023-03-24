ef74772983498bd1c3bf88a2d9ec6b9f1d6e8697
BrandonDeoram <brandondeoram8931@gmail.com>
2023-03-23 22:15:01 -0400

    updated

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,34 +2,62 @@ import React, { useState, useEffect, useRef } from "react";
 import style from "./NavBar.module.css";
 import { useNavigate } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
+
 export default function NavBar() {
   const navigate = useNavigate();
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const avatarRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleOptionClick = (option) => {
+    // Do something with the selected option
+    console.log(`Option ${option} clicked`);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        avatarRef.current &&
+        !avatarRef.current.contains(event.target) &&
+        !event.target.classList.contains(style.dropdown)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <>
       <ul className={style.ul}>
         <li className={style.li}>
-          {/* <button className={style.signIn}>Sign In</button> */}
-          <div>
+          <div ref={avatarRef}>
             <Avatar
               src="https://img.zorores.com/_r/100x100/100/avatar/zoro_normal/av-zz-05.jpeg"
               onClick={toggleMenu}
-              style={{ cursor: "pointer", margin: "10px" }}
+              style={{
+                cursor: "pointer",
+                margin: "10px",
+                height: "48px",
+                width: "48px",
+              }}
             ></Avatar>
             {isMenuOpen && (
               <div
-                className="dropdown-menu"
-                style={{ position: "absolute",right: "0" }}
+                className={style.dropdown}
+                style={{ position: "absolute", right: "0", zIndex: "500" }}
               >
                 <ul>
-                  <li>Option 1</li>
-                  <li>Option 2</li>
-                  <li>Option 3</li>
+                  <li onClick={() => handleOptionClick(1)}>WatchList</li>
+                  <li onClick={() => handleOptionClick(2)}>Logout</li>
                 </ul>
               </div>
             )}
