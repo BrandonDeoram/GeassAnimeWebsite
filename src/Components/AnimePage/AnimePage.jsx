4b490d2986acf0a33badcdcf84bf79e3a2fceaa0
BrandonDeoram <brandondeoram8931@gmail.com>
2023-04-07 17:54:57 -0400

    updated comm

--- src/Components/AnimePage/AnimePage.jsx
+++ src/Components/AnimePage/AnimePage.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import styles from "../Pages/Top.module.css";
-export default function AnimePage() {
+export default function AnimePage({ anime }) {
   return (
     <div className={styles.container}>
-      <img
-        src="https://i.kinja-img.com/gawker-media/image/upload/c8b7d39a00d62c26f4ec2f8fc9986cda.jpg"
-        className={styles.backgroundAnime}
-      />
       <div className={styles.middleContainer}>
-        <h3 className={styles.h3}>Naruto</h3>
-        <p className={styles.description}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          adipisci suscipit necessitatibus cum est repudiandae hic eaque maiores
-          vero, debitis ipsam modi accusantium inventore quisquam cupiditate
-          quidem, iure autem quae. Asperiores dignissimos odit quas debitis
-          facilis impedit veniam eaque adipisci. Quam asperiores porro minus,
-          velit voluptas nulla debitis dolore recusandae possimus non. Enim,
-          esse ducimus.
-        </p>
+        <h3 className={styles.h3}>{anime.title}</h3>
+        <p className={styles.description}>{`${anime.description
+          .split(" ")
+          .slice(0, 100)
+          .join(" ")}...`}</p>
       </div>
+      <img src={anime.image} className={styles.backgroundAnime} />
     </div>
   );
 }
--- src/Components/Pages/Top.jsx
+++ src/Components/Pages/Top.jsx
@@ -5,7 +5,22 @@ import Button from "@mui/material/Button";
 import { ArrowBack } from "@material-ui/icons";
 export default function Top() {
   //get top anime data
-
+  let topAnimeData = [
+    {
+      title: "Naruto",
+      image:
+        "https://i.kinja-img.com/gawker-media/image/upload/c8b7d39a00d62c26f4ec2f8fc9986cda.jpg",
+      description:
+        "Moments prior to Naruto Uzumakis birth, a huge demon known as the Kyuubi, the Nine-Tailed Fox, attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the Kyuubis rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto Now, Naruto is a hyperactive and knuckle headed ninja still living in Konohagakure. Shunned because of the Kyuubi inside him, Naruto struggles to find his place in the village, while his burning desire to become the Hokage of Konohagakure leads him not only to some great new friends, but also some deadly foes",
+    },
+    {
+      title: "Demon Slayer",
+      image:
+        "https://wallpapers.com/images/featured/vy37autfo82jvft7.jpg",
+      description:
+        "Ever since the death of his father, the burden of supporting the family has fallen upon Tanjirou Kamados shoulders. Though living impoverished on a remote mountain, the Kamado family are able to enjoy a relatively peaceful and happy life. One day, Tanjirou decides to go down to the local village to make a little money selling charcoal. On his way back, night falls, forcing Tanjirou to take shelter in the house of a strange man, who warns him of the existence of flesh-eating demons that lurk in the woods at night.When he finally arrives back home the next day, he is met with a horrifying sight—his whole family has been slaughtered. Worse still, the sole survivor is his sister Nezuko, who has been turned into a bloodthirsty demon. Consumed by rage and hatred, Tanjirou swears to avenge his family and stay by his only remaining sibling. Alongside the mysterious group calling themselves the Demon Slayer Corps, Tanjirou will do whatever it takes to slay the demons and protect the remnants of his beloved sisters humanity.",
+    },
+  ];
   return (
     <div className={styles.wrapper}>
       <Button
@@ -18,10 +33,9 @@ export default function Top() {
       >
         <ArrowBack className={styles.buttonColor} />
       </Button>
-
-      <AnimePage></AnimePage>
-
-      <AnimePage></AnimePage>
+      {topAnimeData.map((anime) => (
+        <AnimePage key={anime.title} anime={anime}></AnimePage>
+      ))}
     </div>
   );
 }
